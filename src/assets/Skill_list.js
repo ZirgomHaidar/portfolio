bc446eda7bb553107cbfd41e843aeb8d54c750e0
ZirgomHaidar <zirgomhaidar17@gmail.com>
2024-06-10 15:27:44 +0530

    Add My-Skills Component
    - Use InfiniteMovingCards from Aceternity UI and Customise it for my usage.
    - Add a animation prop in tailwind config file to animate moving cards.

---
+++ src/assets/Skill_list.js
@@ -0,0 +1,78 @@
+import css from "./CSS3.svg";
+import github from "./github.svg";
+import gitlab from "./gitlab.svg";
+import html from "./HTML5.svg";
+import js from "./js.svg";
+import mongodb from "./mongodb.svg";
+import MySQL from "./MySQL_logo.svg";
+import react from "./React_Logo.svg";
+import sql from "./sql.svg";
+import Tailwind from "./Tailwind_CSS.svg";
+import c from "./C.svg";
+import cpp from "./C++.svg";
+import java from "./java.svg";
+import python from "./python.svg";
+import rust from "./Rust.svg";
+
+export const my_skills = [
+  {
+    s_name: "C",
+    s_pic: c,
+  },
+  {
+    s_name: "C++",
+    s_pic: cpp,
+  },
+  {
+    s_name: "Java",
+    s_pic: java,
+  },
+  {
+    s_name: "Python",
+    s_pic: python,
+  },
+  {
+    s_name: "Rust",
+    s_pic: rust,
+  },
+  {
+    s_name: "HTML",
+    s_pic: html,
+  },
+  {
+    s_name: "CSS",
+    s_pic: css,
+  },
+  {
+    s_name: "JavaScript",
+    s_pic: js,
+  },
+  {
+    s_name: "React JS",
+    s_pic: react,
+  },
+  {
+    s_name: "Tailwind CSS",
+    s_pic: Tailwind,
+  },
+  {
+    s_name: "Github",
+    s_pic: github,
+  },
+  {
+    s_name: "Gitlab",
+    s_pic: gitlab,
+  },
+  {
+    s_name: "SQL",
+    s_pic: sql,
+  },
+  {
+    s_name: "MySQL",
+    s_pic: MySQL,
+  },
+  {
+    s_name: "Mongo DB",
+    s_pic: mongodb,
+  },
+];
